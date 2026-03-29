3f5d5e9dd9481a63347d800a6305ccb53f57d9ca
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2026-03-29 10:32:59 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import { redirect } from "next/navigation"
 
 import { DEFAULT_LOCALE } from "@/lib/constants"
 
-export default function RedirectPage() {
+export default function RedirectPage(1) {
   redirect(DEFAULT_LOCALE)
 }
